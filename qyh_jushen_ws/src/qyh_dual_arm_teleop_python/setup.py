b992bb965870a50f501c54444f0d94afddbda6c6
qinyinghao <jsqinyinghao@live.com>
2025-12-30 09:47:25 +0800

    feat: Add launch and configuration files for dual arm teleoperation

--- qyh_jushen_ws/src/qyh_dual_arm_teleop_python/setup.py
+++ qyh_jushen_ws/src/qyh_dual_arm_teleop_python/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/qyh_dual_teleop_launch.py']),
+        ('share/' + package_name + '/config', ['config/left.yaml', 'config/right.yaml']),
     ],
     install_requires=['setuptools', 'numpy', 'scipy'],
     zip_safe=True,
--- qyh_jushen_ws/src/test.md
+++ qyh_jushen_ws/src/test.md
@@ -7,7 +7,8 @@ source ~/qyh_jushen_ws/qyh_jushen_ws/install/setup.bash
 
 ros2 run qyh_vr_bridge  vr_bridge_node
 
-ros2 run qyh_dual_arm_teleop_python qyh_teleop 
+<!-- ros2 run qyh_dual_arm_teleop_python qyh_teleop  -->
+ros2 launch qyh_dual_arm_teleop_python qyh_dual_teleop_launch.py
 
 ~/qyh_jushen_ws/qyh_jushen_ws/StartServo.sh
 
