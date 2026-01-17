42bba6fe284cd23d19db25b18e8d8f9a06c1f419
qinyinghao <jsqinyinghao@live.com>
2026-01-17 10:31:52 +0800

    feat: update camera launch configuration for improved device handling and synchronization

--- src/OrbbecSDK_ROS2/orbbec_camera/launch/qyh.launch.py
+++ src/OrbbecSDK_ROS2/orbbec_camera/launch/qyh.launch.py
@@ -12,16 +12,17 @@ def generate_launch_description():
 
     head_camera = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, "gemini_330_series.launch.py")
+            os.path.join(launch_file_dir, 'gemini_330_series.launch.py')
         ),
-        launch_arguments={                     
-            'enumerate_net_device': 'true',
+        launch_arguments={
             'camera_name': 'head_camera',
-            'device_num': '1',
-            'sync_mode': 'standalone',  # 单相机使用 standalone 模式，无需硬件触发
+            'usb_port': '2-3.1',
+            'device_num': '3',
+            'sync_mode': 'standalone',
             'config_file_path': config_file_path,
-            'camera_serial': 'CPE895300029',
-        }.items(),
+            'camera_serial': 'CP0HC5300021'
+
+        }.items()
     )
 
     right_camera = IncludeLaunchDescription(
@@ -31,7 +32,7 @@ def generate_launch_description():
         launch_arguments={
             'camera_name': 'right_camera',
             'usb_port': '2-3.4.2',
-            'device_num': '2',
+            'device_num': '3',
             'sync_mode': 'software_triggering',
             'config_file_path': config_file_path,
             'camera_serial': 'CP0BB530003J'
@@ -46,17 +47,17 @@ def generate_launch_description():
         launch_arguments={
             'camera_name': 'left_camera',
             'usb_port': '2-3.4.1',
-            'device_num': '2',
-            'sync_mode': 'standalone',  # 单相机使用 standalone 模式，无需硬件触发
+            'device_num': '3',
+            'sync_mode': 'software_triggering',  # 单相机使用 standalone 模式，无需硬件触发
             'config_file_path': config_file_path,
             'camera_serial': 'CP0BB53000AT'
         }.items()
     )
     
-    ld = LaunchDescription([
-        TimerAction(period=1.0, actions=[GroupAction([head_camera])]), # The primary camera should be launched at last
-        # TimerAction(period=5.0, actions=[GroupAction([right_camera])]), # The primary camera should be launched at last
-        # TimerAction(period=10.0, actions=[GroupAction([left_camera])]), # The primary camera should be launched at last
+    ld = LaunchDescription([        
+        TimerAction(period=0.1, actions=[GroupAction([left_camera])]), # The primary camera should be launched at last
+        TimerAction(period=5.0, actions=[GroupAction([right_camera])]), # The primary camera should be launched at last
+        TimerAction(period=10.0, actions=[GroupAction([head_camera])]), # The primary camera should be launched at last
     ])
 
     return ld
